8e913ba9300572df50c01372bd5b3273b0e4ad5d
LYS312 <lyssilis312@gmail.com>
2023-02-24 04:46:32 +0000

    created user and staff models

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from users_app.models import User
 # Create your models here.
+
+class Staff (models.Model):
+    staff_ID= models.IntegerField(primark_key=True, unique=True)
+    faculty= models.CharField(max_length=100)
+    department= models.CharField(max_length=100)
+    courses= models.JSONField()
+    users= models.ForeignKey(User, on_delete=models.CASCADE)
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User (AbstractUser):
+    ACCESS_TYPE= [
+        ('STAFF', 'Staff'),
+        ('STUDENT', 'Student')
+    ]
+    
+    access=models.CharField(max_length=7, choices=ACCESS_TYPE)
+    email = models.EmailField(unique=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
