501b407c1e47891a5720a9d178556a84da7ec99b
LYS312 <lyssilis312@gmail.com>
2023-02-24 04:56:38 +0000

    create student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users_app.models import User
 
 # Create your models here.
+
+class Student (models.Model):
+    student_ID= models.IntegerField(primark_key=True, unique=True)
+    faculty= models.CharField(max_length=100)
+    department= models.CharField(max_length=100)
+    courses= models.JSONField()
+    bio= models.TextField()
+    users= models.ForeignKey(User, on_delete=models.CASCADE)
