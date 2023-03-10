006e2f24da70a5f3eed65dafc6d08af078005967
Michelle <michelle.01.liu@outlook.com>
2023-03-10 20:16:15 +0000

    Create staff registration form

--- base/forms.py
+++ base/forms.py
@@ -2,15 +2,31 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserRegisterForm(UserCreationForm):
+class StaffRegistrationForm(UserCreationForm):
+    school = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(StaffRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        staff = Staff.objects.create(user=user, school=self.cleaned_data['school'])
+        return user
+
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
 
 
-class StaffRegisterForm(forms.ModelForm):
-    class Meta:
-        model = Staff
-        fields = ['school']
+# class StaffRegisterForm(forms.ModelForm):
+#     class Meta:
+#         model = Staff
+#         fields = ['school']
