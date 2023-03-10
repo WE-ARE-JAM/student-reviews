26c6f3629e81e48b874060be1737cac95bd7f449
Michelle <michelle.01.liu@outlook.com>
2023-03-10 20:40:06 +0000

    Edit urls

--- student_tracker/urls.py
+++ student_tracker/urls.py
@@ -19,6 +19,4 @@ from ..base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
 ]
