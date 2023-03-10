916458ece6cddbb6e0d5eb2f928d1a05d6e60de1
Michelle <michelle.01.liu@outlook.com>
2023-03-10 20:21:16 +0000

    Edit register staff function

--- base/views.py
+++ base/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm, StaffRegisterForm
+from .forms import StaffRegistrationForm
 
 # Create your views here.
 
-def register(request):
+
+def register_staff(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        staff_reg_form = StaffRegisterForm(request.POST)
-        if form.is_valid() and staff_reg_form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            staff_reg_form = StaffRegisterForm(request.POST, instance=user.staff)
-            staff_reg_form.full_clean()
-            staff_reg_form.save()
-            messages.success(request, f'Your account has been successfully created!')
+        form = StaffRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('login')
     else:
-        form = UserRegisterForm()
-        staff_reg_form = StaffRegisterForm()
-        context = {
-            'form' : form,
-            'staff_reg_form' : staff_reg_form
-        }
-    return render(request, 'base/templates/register.html', context)
+        form = StaffRegistrationForm()
+    return render(request, 'templates/register.html', {'form': form})
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         staff_reg_form = StaffRegisterForm(request.POST)
+#         if form.is_valid() and staff_reg_form.is_valid():
+#             user = form.save()
+#             user.refresh_from_db()
+#             staff_reg_form = StaffRegisterForm(request.POST, instance=user.staff)
+#             staff_reg_form.full_clean()
+#             staff_reg_form.save()
+#             messages.success(request, f'Your account has been successfully created!')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#         staff_reg_form = StaffRegisterForm()
+#         context = {
+#             'form' : form,
+#             'staff_reg_form' : staff_reg_form
+#         }
+#     return render(request, 'base/templates/register.html', context)
 
 def login(request):
-    return render(request, 'base/templates/login.html')
+    return render(request, 'templates/login.html')
