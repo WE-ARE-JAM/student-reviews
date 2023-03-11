1c2d2e7ee6512036db99bd2aeab84f140c2764a3
Michelle <michelle.01.liu@outlook.com>
2023-03-11 18:12:57 +0000

    Change school field to dropdown list on form

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Staff
+from .models import Staff, School
 
 class StaffRegistrationForm(UserCreationForm):
-    school = forms.CharField(max_length=100, required=True)
+    school = forms.ModelChoiceField(queryset=School.objects.all(), required=True)
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=32, required=True)
     last_name = forms.CharField(max_length=32, required=True)
@@ -19,6 +19,7 @@ class StaffRegistrationForm(UserCreationForm):
         if commit:
             user.save()
         staff = Staff.objects.create(user=user, school=self.cleaned_data['school'])
+        staff.save()
         return user
 
 # class UserRegisterForm(UserCreationForm):
