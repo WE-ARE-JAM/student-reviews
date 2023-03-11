43ad776b24922761998a4d6f97566ed38e1bceeb
Michelle <michelle.01.liu@outlook.com>
2023-03-11 01:27:49 +0000

    Add Staff import to forms.py

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Staff
 
 class StaffRegistrationForm(UserCreationForm):
     school = forms.CharField(max_length=100, required=True)
