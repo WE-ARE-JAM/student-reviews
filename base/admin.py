49a8f9deab3856540fa811db49ae144a55a1cc3a
Michelle <michelle.01.liu@outlook.com>
2023-03-11 20:25:08 +0000

    Register School model to admin

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import User, Student, Staff, Review, Vote, Staff_Inbox, Endorsement
+from .models import User, School, Student, Staff, Review, Vote, Staff_Inbox, Endorsement
 # Register your models here.
 
 admin.site.unregister(User)
 admin.site.register(User)
+admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Staff)
 admin.site.register(Review)
