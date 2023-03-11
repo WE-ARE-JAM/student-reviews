be03a538be8ff233fb6aa92bf8cb76ddc4a7e4b5
Michelle <michelle.01.liu@outlook.com>
2023-03-11 01:28:47 +0000

    Unregister and re-register User model

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import User, Student, Staff, Review, Vote, Staff_Inbox, Endorsement
 # Register your models here.
 
+admin.site.unregister(User)
 admin.site.register(User)
 admin.site.register(Student)
 admin.site.register(Staff)
