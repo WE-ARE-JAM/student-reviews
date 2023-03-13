f4268f0cc0875482714e34c319b761575fd05b70
Michelle <michelle.01.liu@outlook.com>
2023-03-13 02:59:32 +0000

    Add admin registration form to forms.py

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User, Group
-from .models import Staff, School
+from .models import Admin, Staff, School
+
+class AdminRegistrationForm(UserCreationForm):
+    school = forms.ModelChoiceField(queryset=School.objects.all(), required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=32, required=True)
+    last_name = forms.CharField(max_length=32, required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(AdminRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        admin = Admin.objects.create(user=user, school=self.cleaned_data['school'])
+        admin.save()
+
+        # adding user to ADMIN group
+        admin_group, created = Group.objects.get_or_create(name="ADMIN")
+        user.groups.add(admin_group)
+
+        return user
+
 
 class StaffRegistrationForm(UserCreationForm):
     school = forms.ModelChoiceField(queryset=School.objects.all(), required=True)
@@ -24,7 +49,7 @@ class StaffRegistrationForm(UserCreationForm):
         # adding user to STAFF group
         staff_group, created = Group.objects.get_or_create(name="STAFF")
         user.groups.add(staff_group)
-        
+
         return user
 
 # class UserRegisterForm(UserCreationForm):
