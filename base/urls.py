21f75078fdcfddc4131278163ec2ef942f3f1f03
Michelle <michelle.01.liu@outlook.com>
2023-03-14 03:05:32 +0000

    Fix urls

--- base/urls.py
+++ base/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
-    path('', views.login_request, name='login'),
+    path('', views.index, name='index'),
     path('register', views.staff_register, name='register'),
     path('login', views.login_request, name='login'),
     path('logout', views.logout_request, name='logout'),
-    path('home', views.admin_register, name='superuser-home'),
-    path('home', views.staff_home, name='staff-home'),
-    path('home', views.admin_home, name='admin-home'),
+    path('admin-register', views.admin_register, name='superuser-home'),
+    path('profile', views.staff_home, name='staff-home'),
+    path('dashboard', views.admin_home, name='admin-home'),
+    path('unauthorized', views.unauthorized, name='unauthorized'),
 ]
