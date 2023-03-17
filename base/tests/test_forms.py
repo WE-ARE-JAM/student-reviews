843cfa60596d6ca025b5b5e4bae6bd30c695369a
joshuapj <joshpersad180@gmail.com>
2023-03-17 05:26:52 +0000

    added tests for staff model and form

--- base/tests/test_forms.py
+++ base/tests/test_forms.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import Group, User
 from django.urls import reverse
-from base.models import Admin, School
-from base.forms import AdminRegistrationForm
+from base.models import Admin, School, School, Staff
+from base.forms import AdminRegistrationForm, StaffRegistrationForm
 
 
 class AdminRegistrationFormTests(TestCase):
@@ -46,4 +46,100 @@ class AdminRegistrationFormTests(TestCase):
         form = AdminRegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('password2', form.errors)
+
+
+
+
+class StaffRegistrationFormTests(TestCase):
+    def setUp(self):
+        self.school = School.objects.create(name='ASJA')
+    
+
+
+class StaffRegistrationFormTests(TestCase):
+    def setUp(self):
+        self.school = School.objects.create(name="PRES")
+        self.valid_form_data = {
+            'username': 'testuser',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'pres@gmail.com',
+            'password1': 'testpass123',
+            'password2': 'testpass123',
+            'school': self.school.id
+        }
+
+    def test_valid_form(self):
+        form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'pres@gmail.com',
+            'username': 'testuser',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+            'school': self.school.id,
+        }
+        form = StaffRegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'pres@gmail.com')
+        self.assertEqual(user.first_name, 'Test')
+        self.assertEqual(user.last_name, 'User')
+        self.assertEqual(user.groups.count(), 1)
+        self.assertEqual(user.groups.first().name, 'STAFF')
+        staff = Staff.objects.get(user=user)
+        self.assertEqual(staff.school, self.school)
+    
+    def test_invalid_form(self):
+        form_data = {
+            'first_name': '',
+            'last_name': '',
+            'email': 'invalidemail',
+            'username': '',
+            'password1': '',
+            'password2': 'testpassword123',
+            'school': '',
+        }
+        form = StaffRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+
+    def test_blank_form(self):
+        form = StaffRegistrationForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 7)
+
+    def test_password_mismatch(self):
+        form_data = self.valid_form_data.copy()
+        form_data['password2'] = 'differentpassword'
+        form = StaffRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+        self.assertIn('password2', form.errors)
+
+    # def test_existing_email(self):
+    #     user = User.objects.create_user(
+    #         username='existinguser',
+    #         password='existingpassword',
+    #         email=self.valid_form_data['email'],
+    #     )
+    #     form = StaffRegistrationForm(data=self.valid_form_data)
+    #     self.assertFalse(form.is_valid())
+    #     self.assertEqual(len(form.errors), 1)
+    #     self.assertIn('email', form.errors)
+
+    def test_group_added(self):
+        form = StaffRegistrationForm(data=self.valid_form_data)
+        user = form.save()
+        staff_group = Group.objects.get(name='STAFF')
+        self.assertTrue(user.groups.filter(name=staff_group.name).exists())
+
+    def test_staff_created(self):
+        form = StaffRegistrationForm(data=self.valid_form_data)
+        user = form.save()
+        staff = Staff.objects.get(user=user)
+        self.assertEqual(staff.school, self.school)
+
 
--- base/tests/test_models.py
+++ base/tests/test_models.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import Group, User
 from django.urls import reverse
-from base.models import Admin, School
+from base.models import Admin, School, Staff
 from base.forms import AdminRegistrationForm
 
 class AdminModelTests(TestCase):
     def setUp(self):
-        self.school = School.objects.create(name='Test School')
+        self.school = School.objects.create(name='ASJA')
         self.user = User.objects.create_user(
             email='asja@gmail.com',
             username='user',
@@ -17,8 +17,24 @@ class AdminModelTests(TestCase):
         self.admin = Admin.objects.create(user=self.user, school=self.school)
 
     def test_str(self):
-        self.assertEqual(str(self.admin), 'Test User : Test School')
+        self.assertEqual(str(self.admin), 'Test User : ASJA')
 
     def test_user_deletion_cascades(self):
         self.user.delete()
         self.assertFalse(Admin.objects.filter(pk=self.admin.pk).exists())
+
+
+class StaffModelTests(TestCase):
+    def setUp(self):
+        self.school = School.objects.create(name='ASJA')
+        self.user = User.objects.create_user(
+            username='johndoe',
+            email='johndoe@gmail.com',
+            password='testpassword123',
+            first_name='John',
+            last_name='Doe'
+        )
+        self.staff = Staff.objects.create(user=self.user, school=self.school)
+    
+    def test_staff_model_str(self):
+        self.assertEqual(str(self.staff), 'John Doe : ASJA')
