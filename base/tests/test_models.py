ab369348a2b6819588d6db445cd6f28a6804aece
joshuapj <joshpersad180@gmail.com>
2023-03-18 00:36:31 +0000

    added tests for student model

--- base/tests/test_models.py
+++ base/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.db import models
 from django.contrib.auth.models import Group, User
 from django.urls import reverse
-from base.models import Admin, School, Staff
+from base.models import Admin, School, Staff, Student
 from base.forms import AdminRegistrationForm
 
 class AdminModelTests(TestCase):
@@ -38,3 +39,38 @@ class StaffModelTests(TestCase):
     
     def test_staff_model_str(self):
         self.assertEqual(str(self.staff), 'John Doe : ASJA')
+
+
+class StudentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.school = School.objects.create(name='Test School')
+        cls.student = Student.objects.create(name='John Doe', school=cls.school)
+
+    def test_name_label(self):
+        student = StudentModelTest.student
+        field_label = student._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_active_default(self):
+        student = StudentModelTest.student
+        self.assertTrue(student.active)
+
+    def test_profile_pic_null(self):
+        student = StudentModelTest.student
+        self.assertTrue(student._meta.get_field('profile_pic').null)
+
+    def test_str_method(self):
+        student = StudentModelTest.student
+        expected_string = f"{student.name} : {student.school.name}"
+        self.assertEquals(str(student), expected_string)
+
+    def test_school_foreign_key(self):
+        student = StudentModelTest.student
+        self.assertIsInstance(student._meta.get_field('school'), models.ForeignKey)
+        self.assertEquals(student.school, StudentModelTest.school)
+    
+
+
+
