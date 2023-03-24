cd6940225ee35e896c143bcd192ca62f87ae946c
joshuapj <joshpersad180@gmail.com>
2023-03-24 15:19:32 +0000

    testing changes

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -5,61 +5,126 @@ from base.models import School, Admin, Staff, Student
 
 
 class TestViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.admin_group = Group.objects.create(name='ADMIN')
+        cls.staff_group = Group.objects.create(name='STAFF')
+        cls.school = School.objects.create(name='Test School')
 
-    def setUp(self):
-        self.client = Client()
-        self.admin_group = Group.objects.create(name='ADMIN')
-        self.staff_group = Group.objects.create(name='STAFF')
-        self.school = School.objects.create(name='Test School')
-
-        self.admin_user = User.objects.create_user(
+        cls.admin_user = User.objects.create_user(
+            first_name = 'Test',
+            last_name = 'Admin',
             username='admin',  
             email='admin@test.com', 
             password='password123'
         )
 
-        self.admin_user.groups.add(self.admin_group)
+        cls.admin_user.groups.add(cls.admin_group)
 
-        self.admin = Admin.objects.create(
-            user=self.admin_user, 
-            school=self.school
+        cls.admin = Admin.objects.create(
+            user=cls.admin_user, 
+            school=cls.school
         )
 
-        self.staff_user = User.objects.create_user(
+        cls.staff_user = User.objects.create_user(
+            first_name = 'Test',
+            last_name = 'Staff',
             username='staff', 
             email='staff@test.com', 
             password='password123'
         )
-        self.staff_user.groups.add(self.staff_group)
+        cls.staff_user.groups.add(cls.staff_group)
 
-        self.staff = Staff.objects.create(
-            user=self.staff_user, 
-            school=self.school
+        cls.staff = Staff.objects.create(
+            user=cls.staff_user, 
+            school=cls.school
         )
+        print('User count', User.objects.count())
+        print('Admin count', Admin.objects.count())
+        print('Staff count', Staff.objects.count())
 
     def test_admin_login_view(self):
-        # admin login, redirect to admin-home
-        response = self.client.post(reverse('base:login'), 
-            {
-                'username': 'admin', 
-                'password': 'password123'
-            }
-        )
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('base:admin-home'))
+        self.client.login(username='admin', password='password123')
+        response = self.client.get(reverse('base:admin-home'))
+        self.assertEqual(response.status_code, 200)
+        self.client.logout()
 
     def test_staff_login_view(self):
-        # staff login, redirect to staff-home
-        response = self.client.post(reverse('base:login'), 
+        self.client.login(username='staff', password='password123')
+        response = self.client.get(reverse('base:staff-home'))
+        self.assertEqual(response.status_code, 200)
+        self.client.logout()
+
+    def test_staff_registration_view(self):
+        self.client.get(reverse('base:register'))
+        response = self.client.post(reverse('base:register'), {
+            'first_name' : 'Test',
+            'last_name' : 'Stafftwo',
+            'username' :'staff2',  
+            'email' : 'staff2@test.com', 
+            'password' : 'password123',
+            'school' : self.school
+            }
+        )
+        print(response)
+        self.assertRedirects(response, reverse('base:login'))
+        self.client.post(reverse('base:login'), 
             {
-                'username': 'staff', 
+                'username': 'staff2', 
                 'password': 'password123'
             }
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('base:staff-home'))
+        response = self.client.get(reverse('base:staff-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Staff.objects.count(), 2)
 
 
-     
+# def test_staff_registration_view(self):
+    #     response = self.client.post(reverse('base:register'), {
+    #         'username' :'staff2',  
+    #         'email' : 'staff2@test.com', 
+    #         'password' : 'password123'
+    #         }
+    #     )
+    #     self.assertRedirects(response, reverse('base:login'))
+    #     self.assertEqual(User.objects.count(), 3)
+    #     self.assertEqual(Staff.objects.count(), 2)
+
+    # def test_admin_login_view(self):
+    #     # admin login, redirect to admin-home
+    #     response = self.client.post(reverse('base:login'), 
+    #         {
+    #             'username': 'admin', 
+    #             'password': 'password123'
+    #         }
+    #     )
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertRedirects(response, reverse('base:admin-home'))
+
+    # def test_staff_login_view(self):
+    #     # staff login, redirect to staff-home
+    #     response = self.client.post(reverse('base:login'), 
+    #         {
+    #             'username': 'staff', 
+    #             'password': 'password123'
+    #         }
+    #     )
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertRedirects(response, reverse('base:staff-home'))
+
+    # def test_staff_registration_view(self):
+    #     response = self.client.post(reverse('base:register'), {
+    #         'username' :'staff2',  
+    #         'email' : 'staff2@test.com', 
+    #         'password' : 'password123'
+    #         }
+    #     )
+    #     self.assertRedirects(response, reverse('base:login'))
+    #     self.assertEqual(User.objects.count(), 3)
+    #     self.assertEqual(Staff.objects.count(), 2)
+
+
+
 
    
