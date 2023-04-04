d0a04f4742e88bfee64419cd7d4a26ad337515a5
joshuapj <joshpersad180@gmail.com>
2023-04-04 15:51:46 +0000

    added test for staff registration view

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -1,29 +1,112 @@
-from django.contrib.auth.models import User
-from django.test import Client, TestCase
+import json
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
-from django.utils.http import urlencode
-from base.models import Review
-from base.views import staff_home, student_profile, create_review
-import base.urls
+from django.contrib.auth.models import User, Group, AnonymousUser
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
+from base.models import School, Admin, Staff, Student
+from base.forms import StaffRegistrationForm
+from base.views import *
 
-class HomepageTests(TestCase):
+
+class ViewTests(TestCase):
+
     def setUp(self):
-        self.school = School.objects.create(name='ASJA')
-        self.user = User.objects.create_user(
-            email='asja@gmail.com',
-            username='user',
-            first_name='Test',
-            last_name='User',
-            password='testpassword'
-        )
-        self.admin = Admin.objects.create(user=self.user, school=self.school)
+        self.factory = RequestFactory()
         
+        self.admin_group = Group.objects.create(name='ADMIN')
+        self.staff_group = Group.objects.create(name='STAFF')
+        self.school = School.objects.create(name='Test School')
+        self.admin_user = User.objects.create_user(
+            first_name = 'Test',
+            last_name = 'Admin',
+            username='admin',  
+            email='admin@test.com', 
+            password='password123', 
+        )
+        self.admin_user.groups.add(self.admin_group)
+        self.admin = Admin.objects.create(
+            user=self.admin_user, 
+            school=self.school
+        )
 
-
+        self.staff_user = User.objects.create_user(
+            first_name = 'Test',
+            last_name = 'Staff',
+            username='staff', 
+            email='staff@test.com', 
+            password='password123'
+        )
+        self.staff = Staff.objects.create(
+            user=self.staff_user, 
+            school=self.school
+        )
+        self.staff_user.groups.add(self.staff_group)
+        
     def test_login_available_by_name(self):
-        response = self.client.get(reverse('base:login'))
+        client = Client()
+        response = client.get(reverse('base:login'))
         self.assertEqual(response.status_code, 200)
     
     def test_staff_register_available_by_name(self):
-        response = self.client.get(reverse('base:register'))
+        client = Client()
+        response = client.get(reverse('base:register'))
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_login_view(self):
+        client = Client()
+        client.login(username='admin', password='password123')
+        response = client.get(reverse('base:admin-home'))
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+    def test_staff_login_view(self):
+        client = Client()
+        client.login(username='staff', password='password123')
+        response = client.get(reverse('base:staff-home'))
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+
+    def test_staff_registration_view(self):
+        form_data = {
+            'first_name': 'Harry',
+            'last_name': 'Doe',
+            'username': 'staff2',
+            'email': 'staff2@test.com',
+            'password1': 'aMuchMoreComplicatedPassword',
+            'password2': 'aMuchMoreComplicatedPassword',
+            'school': self.school.pk
+        }
+        form = StaffRegistrationForm(data=form_data)
+        if form.is_valid():
+            user = form.save()
+            client = Client()
+            client.login(username='staff2', password='aMuchMoreComplicatedPassword')
+            response = client.get(reverse('base:staff-home'))
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(User.objects.count(), 3)
+            self.assertEqual(Staff.objects.count(), 2)
+            client.logout()
+        else:
+            print(form.errors)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_staff_registration2(self):
+        client = Client()
+        form_data = {
+            'first_name': 'Barry',
+            'last_name': 'Doe',
+            'username': 'staff3',
+            'email': 'staff3@test.com',
+            'password1': 'aMuchMoreComplicatedPassword',
+            'password2': 'aMuchMoreComplicatedPassword',
+            'school': self.school.pk
+        }
+        response = client.post(reverse('base:register'), data=form_data)
+        self.assertEqual(User.objects.count(), 3)
+        self.assertEqual(Staff.objects.count(), 2)
+        client.login(username='staff3', password='aMuchMoreComplicatedPassword')
+        response = client.get(reverse('base:staff-home'))
+        self.assertEqual(response.status_code, 200)
+
