9302e2be7bb237fefa7b2506681e31d8cc1fc007
Michelle <michelle.01.liu@outlook.com>
2023-04-04 14:57:21 +0000

    Make name field in School model unique

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # School Model
 
 class School (models.Model):
-    name= models.CharField(max_length=200, null=False)
+    name = models.CharField(max_length=200, null=False, unique=True)
 
     def __str__(self):
         return '%s' % (self.name)
