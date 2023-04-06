b6b95d57c90dec22b46ddef3b53ba4801c30d0ff
joshuapj <joshpersad180@gmail.com>
2023-04-06 07:02:33 +0000

    started fixing form tests

--- base/tests/test_forms.py
+++ base/tests/test_forms.py
@@ -1,11 +1,11 @@
-# from django.test import TestCase
-# from django.contrib.auth.models import Group, User
-# from django.urls import reverse
-# from base.models import Admin, School, School, Staff, Admin
-# from base.forms import AdminRegistrationForm, StaffRegistrationForm, ReviewForm, UploadCsvForm
-# from django.core.exceptions import ValidationError
-# from django.core.files.uploadedfile import SimpleUploadedFile
-# import io, csv
+from django.test import TestCase
+from django.contrib.auth.models import Group, User
+from django.urls import reverse
+from base.models import Admin, School, School, Staff, Admin
+from base.forms import AdminRegistrationForm, StaffRegistrationForm, ReviewForm, UploadCsvForm
+from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import SimpleUploadedFile
+import io, csv
 
 
 
@@ -26,10 +26,13 @@
 
 #     def test_valid_form(self):
 #         form = AdminRegistrationForm(data=self.form_data)
-#         print(form.errors)
 #         self.assertTrue(form.is_valid())
+
+#     def test_form_created(self):
+#         form = AdminRegistrationForm(data=self.form_data)
 #         user = form.save()
-#         admin = Admin.objects.get(user=user)
+#         print(user)
+#         admin = User.objects.get(user=user)
 #         self.assertEqual(admin.school, self.school)
 #         self.assertTrue(user.groups.filter(name='ADMIN').exists())
 
@@ -54,94 +57,74 @@
 #         self.assertIn('password2', form.errors)
     
 
+class StaffRegistrationFormTest(TestCase):
 
-# class StaffRegistrationFormTests(TestCase):
-#     def setUp(self):
-#         self.school = School.objects.create(name="PRES")
-#         self.valid_form_data = {
-#             'username': 'testuser',
-#             'first_name': 'Test',
-#             'last_name': 'User',
-#             'email': 'pres@gmail.com',
-#             'password1': 'testpass123',
-#             'password2': 'testpass123',
-#             'school': self.school.id
-#         }
+    def setUp(self):
+        self.school = School.objects.create(name="Test School")
 
-#     def test_valid_form(self):
-#         form_data = {
-#             'first_name': 'Test',
-#             'last_name': 'User',
-#             'email': 'pres@gmail.com',
-#             'username': 'testuser',
-#             'password1': 'testpassword123',
-#             'password2': 'testpassword123',
-#             'school': self.school.id,
-#         }
-#         form = StaffRegistrationForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#         user = form.save()
-#         self.assertEqual(user.username, 'testuser')
-#         self.assertEqual(user.email, 'pres@gmail.com')
-#         self.assertEqual(user.first_name, 'Test')
-#         self.assertEqual(user.last_name, 'User')
-#         self.assertEqual(user.groups.count(), 1)
-#         self.assertEqual(user.groups.first().name, 'STAFF')
-#         staff = Staff.objects.get(user=user)
-#         self.assertEqual(staff.school, self.school)
-    
-#     def test_invalid_form(self):
-#         form_data = {
-#             'first_name': '',
-#             'last_name': '',
-#             'email': 'wrongemail',
-#             'username': '',
-#             'password1': '',
-#             'password2': 'pass123',
-#             'school': '',
-#         }
-#         form = StaffRegistrationForm(data=form_data)
-#         self.assertFalse(form.is_valid())
+    def test_form_valid_data(self):
+        form_data = {'first_name': 'Test',
+                     'last_name': 'User',
+                     'username': 'testuser',
+                     'email': 'testuser@example.com',
+                     'password1': 'test_password',
+                     'password2': 'test_password',
+                     'school': self.school.id}
+        form = StaffRegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        staff = form.save()
+        self.assertEqual(staff.user.first_name, 'Test')
+        self.assertEqual(staff.user.last_name, 'User')
+        self.assertEqual(staff.user.username, 'testuser')
+        self.assertEqual(staff.user.email, 'testuser@example.com')
+        self.assertTrue(staff.user.check_password('test_password'))
+        self.assertEqual(staff.school, self.school)
+        group = Group.objects.get(name='STAFF')
+        self.assertTrue(group in staff.user.groups.all())
 
 
+    def test_password_mismatch(self):
+        form_data = {'first_name': 'Test',
+                     'last_name': 'User',
+                     'username': 'testuser',
+                     'email': 'testuser@example.com',
+                     'password1': 'test_password',
+                     'password2': 'test_password',
+                     'school': self.school.id}
+        form_data['password2'] = 'differentpassword'
+        form = StaffRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+        self.assertIn('password2', form.errors)
 
-#     def test_blank_form(self):
-#         form = StaffRegistrationForm(data={})
-#         self.assertFalse(form.is_valid())
-#         self.assertEqual(len(form.errors), 7)
+    def test_form_email_already_exists(self):
+        existing_user = User.objects.create_user(username='existing_user', email='testuser@example.com')
+        form_data = {'first_name': 'Test',
+                     'last_name': 'User',
+                     'username': 'testuser',
+                     'email': 'testuser@example.com',
+                     'password1': 'test_password',
+                     'password2': 'test_password',
+                     'school': self.school.id}
+        form = StaffRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], ['Email already exists'])
 
-#     def test_password_mismatch(self):
-#         form_data = self.valid_form_data.copy()
-#         form_data['password2'] = 'differentpassword'
-#         form = StaffRegistrationForm(data=form_data)
-#         self.assertFalse(form.is_valid())
-#         self.assertEqual(len(form.errors), 1)
-#         self.assertIn('password2', form.errors)
-
-#     def test_existing_email(self):
-#         user = User.objects.create_user(
-#             username='existinguser',
-#             password='existingpassword',
-#             email=self.valid_form_data['email'],
-#         )
-#         form = StaffRegistrationForm(data=self.valid_form_data)
-#         self.assertFalse(form.is_valid())
-#         self.assertIn('email', form.errors)
-#         self.assertEqual(len(form.errors['email']), 1)
-#         self.assertEqual(form.errors['email'][0], 'Email already exists')
-
-#     def test_group_added(self):
-#         form = StaffRegistrationForm(data=self.valid_form_data)
-#         user = form.save()
-#         staff_group = Group.objects.get(name='STAFF')
-#         self.assertTrue(user.groups.filter(name=staff_group.name).exists())
-
-#     def test_staff_created(self):
-#         form = StaffRegistrationForm(data=self.valid_form_data)
-#         user = form.save()
-#         staff = Staff.objects.get(user=user)
-#         self.assertEqual(staff.school, self.school)
-
+    def test_form_missing_data(self):
+        form_data = {'first_name': '',
+                     'last_name': '',
+                     'username': '',
+                     'email': '',
+                     'password1': '',
+                     'password2': '',
+                     'school': None}
+        form = StaffRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['first_name'], ['This field is required.'])
+        self.assertEqual(form.errors['last_name'], ['This field is required.'])
+        self.assertEqual(form.errors['email'], ['This field is required.'])
+        self.assertEqual(form.errors['password1'], ['This field is required.'])
+        self.assertEqual(form.errors['school'], ['This field is required.'])
 
 # class UploadCsvFormTest(TestCase):
 
